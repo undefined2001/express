047454b2e47baeb78fae663146d84b64e00b5091
undefined2001 <theundefined2001@gmail.com>
2023-07-30 03:37:27 +0600

    New Starting

--- models/tasks.models.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-const Task = mongoose.model('Task', { title: String, description:{type:String, required:true} });
-
-module.exports = Task;
--- routes/task.route.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { addTaskForm, saveTask } = require('../controller/task.controller')
-
-router.get("/addtask", addTaskForm);
-
-router.post("/addtask", saveTask);
-
-module.exports = router
